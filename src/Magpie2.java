38bca27656923b324aacdabddda23f2dd4940d6a
robrobrob <robbiegabbie@gmail.com>
2017-11-14 11:51:46 -0500

    added random code to getGreeting

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -18,7 +18,29 @@ public class Magpie2
 	 */
 	public String getGreeting()
 	{
-		return "Hello, let's talk.";
+		final int NUMBER_OF_RESPONSES = 4;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+
+		if (whichResponse == 0)
+		{
+			response = "Interesting, tell me more.";
+		}
+		else if (whichResponse == 1)
+		{
+			response = "Hmmm.";
+		}
+		else if (whichResponse == 2)
+		{
+			response = "Do you really think so?";
+		}
+		else if (whichResponse == 3)
+		{
+			response = "You don't say.";
+		}
+
+		return response;
 	}
 	
 	/**
